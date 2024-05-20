04c55e0a9b997a66927e48036cacf29464364df9
9050nasser <9050nasser@gmail.com>
2024-05-20 14:56:35 +0300

    First Commit

---
+++ garden_lights/garden_lights/report/اجمالي_المبيعات_بالشهور/اجمالي_المبيعات_بالشهور.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Mohammed  and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["اجمالي المبيعات بالشهور"] = {
+	"filters": [
+
+	]
+};
