0998fb86363141dc55c53be756551c0eb09b9420
9050nasser <9050nasser@gmail.com>
2024-05-20 15:38:00 +0300

    move code

--- garden_lights/garden_lights/report/اجمالي_المبيعات_بالشهور/اجمالي_المبيعات_بالشهور.js
+++ garden_lights/garden_lights/report/اجمالي_المبيعات_بالشهور/اجمالي_المبيعات_بالشهور.js
@@ -1,9 +1,23 @@
-// Copyright (c) 2024, Mohammed  and contributors
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 /* eslint-disable */
 
 frappe.query_reports["اجمالي المبيعات بالشهور"] = {
-	"filters": [
-
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			reqd: 1,
+			default: frappe.defaults.get_user_default("Company"),
+		},
+		{
+			fieldname: "year",
+			label: __("Year"),
+			fieldtype: "Data",
+			reqd: 1,
+		},
+		
 	]
 };
